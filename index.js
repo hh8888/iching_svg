a926535a3e18ebf685f35508851d87307b52f828
hh8888 <harry_gx@hotmail.com>
2021-09-20 00:39:00 +1000

    better yinyang

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 //sappDiv.innerHTML = `<h1>JS Starter123</h1>`;
+
+function rotateEl(el) {
+  console.log('rotateEL');
+}
+
+function rotate() {
+  var el = document.getElementById('yinyang');
+  rotateEl(el);
+}
+
+document.addEventListener('load', function (e) {
+  console.log('Load');
+  rotate();
+});
+
+rotate();
