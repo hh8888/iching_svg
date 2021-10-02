56d4bc0ebabedff50a5d7f9966482cfc1c767437
Harry Meng <hugo.meng@gmail.com>
2021-10-02 20:43:49 +1000

    game version2

--- game.js
+++ game.js
@@ -6,8 +6,12 @@ window.onload=function(){
     plusBtn  = document.getElementById("plus"),
     minusBtn = document.getElementById("minus"),
     yaoBtn = document.getElementById("yao"),
+    resetBtn = document.getElementById("reset"),
     minusBtn = document.getElementById("minus"),
     speedText = document.getElementById("speed"),
+    bgDiv = document.getElementById("bgDiv"),
+    yaoCard = document.getElementById("yaoCard"),
+    yaoDetail = document.getElementById("yaoDetail"),
     audio    = document.createElement("audio");
 
 audio.loop = true;
@@ -74,8 +78,7 @@ minusBtn.onclick = function() {
 }
     
 pauseBtn.onclick = function() {
-    gsap.to(rotateCD, { timeScale: 0, duration:3, onComplete: function() { this.pause(); }});
-    speedText.innerHTML=0;
+    spinStop();
   
   pauseTween && pauseTween.kill();
   pauseTween = gsap.to(audio, { 
@@ -87,11 +90,53 @@ pauseBtn.onclick = function() {
   });
 }
 
+function spinStop(){
+    gsap.to(rotateCD, { timeScale: 0, duration:3, onComplete: function() { this.pause(); }});
+    speedText.innerHTML=0;
+}
+
+function showCard(){
+    bgDiv.classList.remove("fg");
+    bgDiv.classList.add("bg");
+    yaoCard.classList.remove("hidden");
+    yaoCard.classList.add("show");
+    bgDiv.classList.add("bg");
+    yaoDetail.classList.remove("hidden");
+    yaoDetail.classList.add("show");
+}
+
 var numYao = 64;
 yaoBtn.onclick = function() {
+    spinStop();
+    drawCard();
+    showCard();
+}
+
+var currentYao = -1;
+function drawCard(){
     var time = new Date().getTime();
     if(speed===0) speed=Math.random();
     var yaoNumber = Math.floor(time*speed)%numYao;
     console.log(yaoNumber);
+    currentYao = yaoNumber;
+
+    document.querySelector("#yaoNumber").innerHTML=currentYao;
+    document.querySelector("#yaoDetail").innerHTML=data[1].detail;
 }
+
+resetBtn.onclick = function(){
+    bgDiv.classList.remove("bg");
+    bgDiv.classList.add("fg");
+    yaoCard.classList.remove("show");
+    yaoCard.classList.add("hidden");
+    yaoDetail.classList.remove("show");
+    yaoDetail.classList.add("hidden");
+}
+
+var data={
+    "1":{
+        "detail":"<p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blahblah blah blah blah blah blah blah blah blah blah ...</p>"
+    }
+}
+
 }
